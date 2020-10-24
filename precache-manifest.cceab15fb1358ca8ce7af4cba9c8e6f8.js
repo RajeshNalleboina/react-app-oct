bd924b1613a8dcc74e12d7690cf6ff9832e91867
unknown <rajeshnalleboina94@gmail.com>
2020-10-24 17:51:16 +0530

    Updates

---
+++ precache-manifest.cceab15fb1358ca8ce7af4cba9c8e6f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba3515f88e8fdac8c5c83ad92b5df83a",
+    "url": "/react-afternoon/index.html"
+  },
+  {
+    "revision": "0b4f54cda33c3e5ec855",
+    "url": "/react-afternoon/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4353c57797e77ca50d92",
+    "url": "/react-afternoon/static/js/2.9c7d434c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-afternoon/static/js/2.9c7d434c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b4f54cda33c3e5ec855",
+    "url": "/react-afternoon/static/js/main.2d8bf1ec.chunk.js"
+  },
+  {
+    "revision": "bf86dc8622e74c63fb0d",
+    "url": "/react-afternoon/static/js/runtime-main.8659e136.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-afternoon/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2369d24d785f348feeb8b6cb8d555864",
+    "url": "/react-afternoon/static/media/man.2369d24d.svg"
+  }
+]);
